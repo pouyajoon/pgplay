03007b276bf4049d566f40e6ff4b6d7606cf0ff8
Pouya <pouyajoon@gmail.com>
2016-08-10 01:09:55 +0200

    v2 moving on map

--- index.js
+++ index.js
@@ -1,169 +1,267 @@
-var Pokeio = require('./Pokemon-GO-node-api/poke.io.js');
+(function() {
+  'use strict';
 
-var loc = {
-	type: 'name',
-	name: 'Paris'
-};
+  var Pokeio, current_pos, pos_lat, pos_lon, pokeio, data, close_distance = 0.039,
+    interval;
 
-var pos_lat = 48.871146;
-var pos_lon = 2.330233;
+  Pokeio = require('./Pokemon-GO-node-api/poke.io.js');
 
-var scribe5 = {
-	type: 'coords',
-	coords: {
-		latitude: pos_lat,
-		longitude: pos_lon,
-		altitude: 0
-	}
-};
+  // 5 rue scribe
+  // pos_lat = 48.871146;
+  // pos_lon = 2.330233;
 
-var current_pos = scribe5;
+  // rue avron
+  pos_lat = 48.852846;
+  pos_lon = 2.409021;
 
-var pokeio = new Pokeio.Pokeio();
-var profile, pokemons = [],
-	all_pokemons = {},
-	data = {
-		forts: [],
-		cforts: [],
-		cdownforts: []
-	};
+  current_pos = {
+    type: 'coords',
+    coords: {
+      latitude: pos_lat,
+      longitude: pos_lon,
+      altitude: 0
+    }
+  };
 
 
+  pokeio = new Pokeio.Pokeio();
+  data = {
+    all_pokemons: {},
+    pokemons: [],
+    profile: null,
+    forts: [],
+    cforts: [],
+    cdownforts: [],
+    // fortsMarker: {}
+    fortsById: {}
+  };
 
-function setupExpress() {
-	var express = require('express');
-	var app = express();
-	var http = require('http').Server(app);
-	var io = require('socket.io')(http);
+  function distance(lat1, lon1, lat2, lon2) {
+    var p, a;
+    p = 0.017453292519943295; // Math.PI / 180
+    a = 0.5 - Math.cos((lat2 - lat1) * p) / 2 +
+      Math.cos(lat1 * p) * Math.cos(lat2 * p) *
+      (1 - Math.cos((lon2 - lon1) * p)) / 2;
+    // console.log('calc', a, Math.cos((lat2 - lat1) * p), lat2 - lat1, lat2, lat1, Math.cos(lat1 * p) * Math.cos(lat2 * p), (1 - Math.cos((lon2 - lon1) * p)) / 2, 12742 * Math.asin(Math.sqrt(a)));
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+  }
 
-	app.get('/', function(req, res) {
-		res.send('<h1>Hello world</h1>');
-	});
+  function getFortBoost(socket, fortId, lat, lon) {
+    pokeio.GetFortSearch(fortId, lat, lon, function(err, fort) {
+      console.log('getFortBoost', err, fort);
+      data.fortsById[fortId].CooldownCompleteMs = fort.cooldown_complete_timestamp_ms;
+      socket.emit('fort-taken', fortId);
+    });
+  }
 
-	app.use(express.static('public'));
+  function takeNearForts(socket) {
+    var fortId, fort, dist,
+      readyTimeStamp;
+    console.log('takeNearForts');
+    for (fortId in data.fortsById) {
+      if (data.fortsById.hasOwnProperty(fortId)) {
+        fort = data.fortsById[fortId];
+        dist = distance(pokeio.playerInfo.latitude, pokeio.playerInfo.longitude, fort.Latitude, fort.Longitude);
+        if (dist < close_distance) {
+          console.log('distance ok', dist, fort.FortId, fort.CooldownCompleteMs);
+          if (fort.CooldownCompleteMs) {
+            readyTimeStamp = parseInt(fort.CooldownCompleteMs.toString(), 10);
+            if (Date.now() > readyTimeStamp) {
+              getFortBoost(socket, fort.FortId, fort.Latitude, fort.Longitude);
+            }
+          } else {
+            getFortBoost(socket, fort.FortId, fort.Latitude, fort.Longitude);
+          }
+        }
+      }
+    }
+    // return nearFortsId();
+  }
 
-	http.listen(3000, function() {
-		console.log('listening on *:3000');
-	});
+  function setupExpress() {
+    var express, app, http, io;
+    express = require('express');
+    app = express();
+    http = require('http').Server(app);
+    io = require('socket.io')(http);
 
-	io.on('connection', function(socket) {
-		console.log('a user connected');
-		socket.on('disconnect', function() {
-			console.log('user disconnected');
-		});
-		socket.on('get-profile', function(callback) {
-			return callback({
-				all: pokeio,
-				all_pokemons: all_pokemons,
-				info: pokeio.playerInfo,
-				profile: profile,
-				pokemons: pokemons,
-				data: data
-			});
-		});
-	});
-}
+    app.use(express.static('public'));
 
-function distance(lat1, lon1, lat2, lon2) {
-	// console.log('distance', lat1, lat2, lon1, lon2);
-	// lat1 = parseFloat(lat1);
-	// lon1 = parseFloat(lon1);
-	// lat2 = parseFloat(lat2);
-	// lon2 = parseFloat(lon2);
+    http.listen(3000, function() {
+      console.log('listening on *:3000');
+    });
 
-	var p = 0.017453292519943295; // Math.PI / 180
+    io.on('connection', function(socket) {
+      console.log('a user connected');
+      socket.on('disconnect', function() {
+        console.log('user disconnected');
+      });
+      socket.on('get-profile', function(callback) {
+        return callback && callback({
+          all: pokeio,
+          info: pokeio.playerInfo,
+          data: data
+        });
+      });
 
-	var a = 0.5 - Math.cos((lat2 - lat1) * p) / 2 +
-		Math.cos(lat1 * p) * Math.cos(lat2 * p) *
-		(1 - Math.cos((lon2 - lon1) * p)) / 2;
-	// console.log('calc', a, Math.cos((lat2 - lat1) * p), lat2 - lat1, lat2, lat1, Math.cos(lat1 * p) * Math.cos(lat2 * p), (1 - Math.cos((lon2 - lon1) * p)) / 2, 12742 * Math.asin(Math.sqrt(a)));
-	return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
-}
+      socket.on('move-to', function(target, callback) {
 
-// function distance(lat1,lon1,lat2,lon2) {
-//   var R = 6371; // Radius of the earth in km
-//   var dLat = deg2rad(lat2-lat1);  // deg2rad below
-//   var dLon = deg2rad(lon2-lon1); 
-//   var a = 
-//     Math.sin(dLat/2) * Math.sin(dLat/2) +
-//     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-//     Math.sin(dLon/2) * Math.sin(dLon/2)
-//     ; 
-//   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-//   var d = R * c; // Distance in km
-//   return d;
-// }
+        if (interval) {
+          clearInterval(interval);
+        }
 
-// function deg2rad(deg) {
-//   return deg * (Math.PI/180)
-// }
+        var points = [],
+          dist = 2 / 1e6,
+          move = dist,
+          source = {
+            lat: pokeio.playerInfo.latitude,
+            lon: pokeio.playerInfo.longitude
+          },
+          total_distance = distance(source.lat, source.lon, target.lat, target.lon);
 
-pokeio.init('pouyapokemon', 'pokemonGO', current_pos, 'google', function(err) {
-	console.error(err);
+        while (Math.abs(source.lat - target.lat) > dist || Math.abs(source.lon - target.lon) > dist) {
+          if (source.lat > target.lat) {
+            source.lat -= move;
+          } else {
+            source.lat += move;
+          }
+          if (source.lon > target.lon) {
+            source.lon -= move;
+          } else {
+            source.lon += move;
+          }
+          points.push({
+            lat: source.lat,
+            lon: source.lon
+          });
+        }
+        console.log(points.length, source, target);
 
-	pokeio.pokemonlist.forEach(function(p) {
-		all_pokemons[p.id] = p;
-	})
+        function doMove() {
+          var start, duration, first, modulo = 0;
+          start = Date.now();
 
-	function getProfile() {
-		pokeio.GetProfile(function(err, p) {
-			// console.log(p);
-			profile = p;
+          interval = setInterval(function() {
+            first = points.shift();
+            if (first) {
+              socket.emit('user-new-position', first);
+              pokeio.SetLocation({
+                type: 'coords',
+                coords: {
+                  latitude: first.lat,
+                  longitude: first.lon,
+                  altitude: 0
+                }
+              }, function(err, res) {
+                if (err) {
+                  console.log('setLocation', err, res);
+                }
+              });
+              if (modulo % 20 === 0) {
 
-		});
-	}
+                takeNearForts(socket);
+              }
+            }
+            modulo += 1;
+            if (points.length === 0) {
+              duration = Date.now() - start;
+              console.log('timing of move', (total_distance * 1000).toFixed(2), duration, total_distance / (duration / (1000 * 60 * 60)), ' km/h');
+              clearInterval(interval);
+              return callback && callback(null);
+            }
+          }, 60);
+        }
 
-	function getInventory() {
-		pokeio.GetInventory(function(err, inventory) {
-			var p;
-			// console.log('get invetenory', err, inventory);
-			inventory.inventory_delta.inventory_items.forEach(function(i) {
-				p = i.inventory_item_data.pokemon;
-				if (p !== null && p.is_egg === null) {
-					pokemons.push(p);
-				}
-			})
-			pokemons = pokemons.sort(function(a, b) {
-				return a.pokemon_id - b.pokemon_id;
-			});
-			pokemons.forEach(function(p) {
-				p.reference = all_pokemons[p.pokemon_id];
-			})
-
-			setupExpress();
-		});
-
-	}
-
-	function getForts() {
-		pokeio.Heartbeat(function(err, hb) {
-			if (err) {
-				console.log(err);
-			}
-			hb.cells.forEach(function(o) {
-				if (o.Fort.length > 0) {
-					data.forts = data.forts.concat(o.Fort);
-					o.Fort.forEach(function(f) {
-						var dist = distance(current_pos.coords.latitude, current_pos.coords.longitude, f.Latitude, f.Longitude);
-						// console.log(f.FortId, f.FortType, f.Enabled, dist, current_pos.coords.latitude, current_pos.coords.longitude, f.Latitude, f.Longitude);
-						console.log(f.FortId, f.FortType, f.Enabled, dist);
-
-						if (dist < 0.02) {
-							data.cforts.push(f);
-						}
-						if (f.CooldownCompleteMs !== null){
-							data.cdownforts.push(f);
-						}
-					})
-				}
-			});
-			console.log(data.forts.length);
-		});
-	}
-
-	// getProfile();
-	getInventory();
-	getForts();
+        doMove();
 
 
-});
+      });
+
+      socket.on('get-user-position', function(callback) {
+        return callback && callback({
+          lat: pokeio.playerInfo.latitude,
+          lon: pokeio.playerInfo.longitude
+        });
+      });
+    });
+  }
+
+
+
+  pokeio.init('pouyapokemon', 'pokemonGO', current_pos, 'google', function(err) {
+    console.log(err);
+
+    pokeio.pokemonlist.forEach(function(p) {
+      data.all_pokemons[p.id] = p;
+    });
+
+    function getProfile() {
+      pokeio.GetProfile(function(err, p) {
+        console.log('GetProfile', err);
+        data.profile = p;
+      });
+    }
+
+    function getInventory() {
+      pokeio.GetInventory(function(err, inventory) {
+        if (err) {
+          return console.log('GetInventory', err);
+        }
+        var p;
+        // console.log('get invetenory', err, inventory);
+        inventory.inventory_delta.inventory_items.forEach(function(i) {
+          p = i.inventory_item_data.pokemon;
+          if (p !== null && p.is_egg === null) {
+            data.pokemons.push(p);
+          }
+        });
+        data.pokemons = data.pokemons.sort(function(a, b) {
+          return a.pokemon_id - b.pokemon_id;
+        });
+        data.pokemons.forEach(function(p) {
+          p.reference = data.all_pokemons[p.pokemon_id];
+        });
+        setupExpress();
+      });
+    }
+
+    function getForts() {
+      pokeio.Heartbeat(function(err, hb) {
+        if (err) {
+          return console.log('Heartbeat', err);
+        }
+
+        hb.cells.forEach(function(o) {
+          if (o.Fort.length > 0) {
+            data.forts = data.forts.concat(o.Fort);
+            o.Fort.forEach(function(f) {
+
+              var dist = distance(current_pos.coords.latitude, current_pos.coords.longitude, f.Latitude, f.Longitude);
+              // console.log(f.FortId, f.FortType, f.Enabled, dist, current_pos.coords.latitude, current_pos.coords.longitude, f.Latitude, f.Longitude);
+              // console.log(f.FortId, f.FortType, f.Enabled, dist);
+              if (f.CooldownCompleteMs !== null) {
+                f.CooldownCompleteMs_TimeStamp = parseInt(f.CooldownCompleteMs.toString(), 10);
+                data.cdownforts.push(f);
+              }
+              if (dist < close_distance) {
+                data.cforts.push(f);
+              }
+              data.fortsById[f.FortId] = f;
+            });
+          }
+        });
+        console.log(data.forts.length, data.cforts.length, data.cdownforts.length);
+      });
+    }
+
+    // getProfile();
+    getInventory();
+    getForts();
+
+
+    // getFortBoost('cde87c1bd69a43dea74dc4756b8856c9.16', 48.85261, 2.408995);
+
+
+  });
+
+}());
--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,168 @@
-var socket = io();
+(function() {
+  'use strict';
+  /*global io, angular, google, moment*/
+
+  var socket, app, map, me, fortMarkers = {};
+  socket = io();
 
 
-var app = angular.module('myApp', [])
-app.controller('MainController', function($scope) {
+  socket.on('user-new-position', function(position) {
+    console.log('user position');
+    me.setPosition(new google.maps.LatLng(position.lat, position.lon));
+  });
 
-  $emit = function(key, callback) {
-    socket.emit(key, function(res) {
-      $scope.$apply(function() {
-        return callback(res);
-      })
+
+  function setMarkerColor(marker, color) {
+    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/' + color + '-dot.png');
+  }
+
+  socket.on('fort-taken', function(fortId) {
+    console.log('fort-taken', fortId);
+    setMarkerColor(fortMarkers[fortId], 'red');
+  });
+
+  // function distance(lat1, lon1, lat2, lon2) {
+  //   var p, a;
+  //   p = 0.017453292519943295; // Math.PI / 180
+  //   a = 0.5 - Math.cos((lat2 - lat1) * p) / 2 +
+  //     Math.cos(lat1 * p) * Math.cos(lat2 * p) *
+  //     (1 - Math.cos((lon2 - lon1) * p)) / 2;
+  //   // console.log('calc', a, Math.cos((lat2 - lat1) * p), lat2 - lat1, lat2, lat1, Math.cos(lat1 * p) * Math.cos(lat2 * p), (1 - Math.cos((lon2 - lon1) * p)) / 2, 12742 * Math.asin(Math.sqrt(a)));
+  //   return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+  // }
+
+
+
+  function addMarker(map, lat, lon, title, color, infoContent) {
+    var marker, infowindow;
+    // console.log('add marker', lat, lon, color);
+    infowindow = new google.maps.InfoWindow({
+      content: '<h5>' + title + '</h5>' + infoContent
+    });
+
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, lon),
+      map: map,
+      title: title,
+      icon: 'http://maps.google.com/mapfiles/ms/icons/' + color + '-dot.png'
+    });
+
+
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+    return marker;
+  }
+
+  // function getListOfPoints(source, target) {
+  //   var points = [],
+  //     dist = 2 / 1e6,
+  //     move = dist,
+  //     total_distance = distance(source.lat, source.lon, target.lat, target.lon);
+
+  //   while (Math.abs(source.lat - target.lat) > dist || Math.abs(source.lon - target.lon) > dist) {
+  //     console.log('still work');
+  //     if (source.lat > target.lat) {
+  //       source.lat -= move;
+  //     } else {
+  //       source.lat += move;
+  //     }
+  //     if (source.lon > target.lon) {
+  //       source.lon -= move;
+  //     } else {
+  //       source.lon += move;
+  //     }
+  //     points.push({
+  //       lat: source.lat,
+  //       lon: source.lon
+  //     });
+  //   }
+  //   console.log(points.length, source, target);
+
+  //   var start = Date.now();
+
+  //   var interval = setInterval(function() {
+  //     var first = points.shift();
+  //     if (first) {
+  //       me.setPosition(new google.maps.LatLng(first.lat, first.lon));
+  //     }
+  //     // console.log('move', first);
+  //     if (points.length === 0) {
+  //       console.log('end of move', me.position.lat() - target.lat, me.position.lng() - target.lon);
+  //       var duration = Date.now() - start;
+  //       console.log('timing', (total_distance * 1000).toFixed(2), duration, total_distance / (duration / (1000 * 60 * 60)));
+  //       clearInterval(interval);
+  //     }
+  //   }, 60);
+  // }
+
+  function setMap(lat, lon) {
+    var options;
+    options = {
+      zoom: 18,
+      center: new google.maps.LatLng(lat, lon),
+      mapTypeControl: false
+    };
+    // init map
+    map = new google.maps.Map(document.getElementById('map'), options);
+    me = addMarker(map, lat, lon, 'myPosition', 'green', 'Me!!');
+    // user_pos = {
+    //   lat: lat,
+    //   lon: lon
+    // };
+
+    google.maps.event.addListener(map, 'click', function(event) {
+      var target = {
+        lat: event.latLng.lat(),
+        lon: event.latLng.lng()
+      };
+      console.log(target);
+      socket.emit('move-to', target, function(res) {
+        console.log('move-to', 'done', res);
+      });
+
+      // getListOfPoints(user_pos, target);
     });
   }
 
 
-  $emit('get-profile', function(res) {
-    $scope.pokemons = res.pokemons;
-    
-    console.log(res);
+
+  // setTimeout(setMap, 500);
+
+  app = angular.module('myApp', []);
+  app.controller('MainController', function($scope) {
+
+    var $emit = function(key, callback) {
+      socket.emit(key, function(res) {
+        $scope.$apply(function() {
+          return callback(res);
+        });
+      });
+    };
+
+    $emit('get-user-position', function(res) {
+      console.log('set map', res);
+      setMap(res.lat, res.lon);
+    });
+
+    $emit('get-profile', function(res) {
+      console.log(res);
+
+      $scope.pokemons = res.data.pokemons;
+
+      res.data.forts.forEach(function(f) {
+        var marker = addMarker(map, f.Latitude, f.Longitude, f.Latitude + ', ' + f.Longitude, 'blue', f.FortId);
+        fortMarkers[f.FortId] = marker;
+        if (f.CooldownCompleteMs && f.CooldownCompleteMs_TimeStamp) {
+          console.log(f.CooldownCompleteMs_TimeStamp);
+          if (Date.now() > f.CooldownCompleteMs_TimeStamp) {
+            console.log(f, Date.now(), parseInt(f.CooldownCompleteMs.toString(), 10));
+            setMarkerColor(marker, 'yellow');
+          }
+        }
+      });
+    });
+
   });
 
-});
+}());
